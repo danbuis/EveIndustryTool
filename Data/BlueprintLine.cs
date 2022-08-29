4d0bc9cb3e9c76eddc582a519fef82e5bf4f0a95
Daniel Buis <Daniel.Buis@milliman.com>
2022-08-29 15:44:42 -0400

    Initial commit

---
+++ Data/BlueprintLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EveIndustryTool.Data
+{
+    public class BlueprintLine
+    {
+        public int typeID { get; set; }
+        public int activityID { get; set; }
+        public int materialTypeID { get; set; }
+        public int quantity { get; set; }
+
+        public BlueprintLine(int type, int activty, int materialType, int quantity)
+        {
+            this.typeID = type;
+            this.activityID = activty;
+            this.materialTypeID = materialType;
+            this.quantity = quantity;
+        }
+    }
+}
---
+++ Data/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EveIndustryTool.Data
+{
+    public class Item
+    {
+        public int typeID { get; set; }
+        public int groupID { get; set; }
+        public string name { get; set; }
+        public string mass { get; set; }
+        public string volume { get; set; }
+        public int portion { get; set; }
+        public int graphicID { get; set; }
+
+        public Item(int type, int group, string name, string mass, string volume, int portion, int graphics)
+        {
+            this.typeID = type;
+            this.groupID = group;
+            this.name = name;
+            this.mass = mass;
+            this.volume = volume;
+            this.portion = portion;
+            this.graphicID = graphics;
+        }
+    }
+}
